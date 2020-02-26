077b1ec940b1bf0415189c04ea3a0803c4d9e946
azad <dev.akazad@hotmail.com>
2020-02-26 21:08:41 +0600

    code uncommented and packacge.json file added

--- promise-chaining.js
+++ promise-chaining.js
@@ -4,25 +4,25 @@
 // WILL BE PASSED INTO THEN HANDLER. IF WE HAVE MULTIPLE THEN HANDLER ONE AFTER ANOTHER. THERE'S
 // NO PROBLEM WITH THIS AND IS KNOWN AS 'PROMISE CHAINING' . LET'S SEE AN EXAMPLE
 
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//    resolve(10)
-//}, 1000);
-// });
+const promiseOne = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 1000);
+});
 
-// promise
-//   .then(result => {
-//     console.log(result); //print 10 from resolve
-//     return result * 2;
-//   })
-//   .then(result => {
-//     console.log(result); //print 20 from prev then handler
-//     return result * 2;
-//   })
-//   .then(result => {
-//     console.log(result); //print 40 from prev then handler
-//     return result * 2;
-//   });
+promiseOne
+  .then(result => {
+    console.log(result); //print 10 from resolve
+    return result * 2;
+  })
+  .then(result => {
+    console.log(result); //print 20 from prev then handler
+    return result * 2;
+  })
+  .then(result => {
+    console.log(result); //print 40 from prev then handler
+    return result * 2;
+  });
 
 // THE WHOLE THING IS WORKING BECAUSE A CALL TO 'PROMISE.THEN()' RETURNS A PROMISE. AND IF WE RETURN ANY VALUE FROM
 // THE FIRST THEN() HANDLER, THAT VALUE WILL BE PASSED AS A PARAMETER OF FULLFILLED PROMSIE(WHICH IS RETURNED FROM)
@@ -30,26 +30,26 @@
 
 //NOW WE WILL DISCUSS ABOUT A COMMON MISTAKE MADE BY PEOPLE WHO ARE NEW AT THIS TOPIC. LET'S SEE AND EXAMPLE
 
-// const promise = new Promise((resolve, reject) => {
-//   setTimeout(() => {
-//    resolve(100)
-//}, 2000);
-// });
+const promiseTwo = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(100);
+  }, 2000);
+});
 
-// promise.then(result => {
-//   console.log(result); //prints 100 on the console
-//   return result * 2;
-// });
+promiseTwo.then(result => {
+  console.log(result); //prints 100 on the console
+  return result * 2;
+});
 
-// promise.then(result => {
-//   console.log(result); //prints 100 but people get confused with 200
-//   return result * 2;
-// });
+promiseTwo.then(result => {
+  console.log(result); //prints 100 but people get confused with 200
+  return result * 2;
+});
 
-// promise.then(result => {
-//   console.log(result); //prints 100 but people get confused with 400
-//   return result * 2;
-// });
+promiseTwo.then(result => {
+  console.log(result); //prints 100 but people get confused with 400
+  return result * 2;
+});
 
 // HERE ALL THEN() HANDLER IS CONNECTED TO ONE SINGLE PROMISE. SO ALL THEN() HANDLER WILL GET THE SAME VALUE THAT IS
 // 100. IN REAL WORLD EXAMPLE, WE RARELY HAVE MULTIPLE THEN() METHOD TO SINGLE PROMISE. CHAINING IS USED MUCH MORE
@@ -57,13 +57,13 @@
 // INDISE A THEN() HANDLER, A PROMISE CAN BE CREATED AND RETURNED. IN THAT CASE, THE NEXT THEN() WILL BE WAITING
 // UNTILL THIS PROMSIE IS SETTLED. LET'S HAVE AN EXAMPLE
 
-const promise = new Promise((resolve, reject) => {
+const promiseThree = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve(2);
   }, 1000);
 });
 
-promise
+promiseThree
   .then(result => {
     console.log(result);
     return new Promise((resolve, reject) => {
